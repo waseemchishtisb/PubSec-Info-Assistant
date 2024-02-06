54006ae2f76271c30add7cac34298ab56d46c8e5
waseemchishtisb <157599104+waseemchishtisb@users.noreply.github.com>
2024-02-06 06:32:44 +0000

    front end changes

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -7,6 +7,7 @@ import openai from "../../assets/openai.svg";
 import { WarningBanner } from "../../components/WarningBanner/WarningBanner";
 import styles from "./Layout.module.css";
 import { Title } from "../../components/Title/Title";
+import veraqor from "../../assets/veraqor.png"
 
 const Layout = () => {
     return (
@@ -15,8 +16,9 @@ const Layout = () => {
                 <WarningBanner />
                 <div className={styles.headerContainer}>
                     <div className={styles.headerTitleContainer}>
-                        <img src={openai} alt="Azure OpenAI" className={styles.headerLogo} />
+                        <img src={veraqor} alt="Azure OpenAI" className={styles.headerLogo} />
                         <h3 className={styles.headerTitle}><Title/></h3>
+                        {/* <h3>Veraqor Information Assistant</h3> */}
                     </div>
                     <nav>
                         <ul className={styles.headerNavList}>
@@ -34,9 +36,9 @@ const Layout = () => {
                     </nav>
                 </div>
             </header>
-            <div className={styles.raibanner}>
+            {/* <div className={styles.raibanner}>
                 <span className={styles.raiwarning}>AI-generated content may be incorrect</span>
-            </div>
+            </div> */}
 
             <Outlet />
 
