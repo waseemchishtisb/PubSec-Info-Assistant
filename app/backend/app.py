8ac294aef43f12e9d82159e7db596e3f5c7d868d
waseemchishtisb <157599104+waseemchishtisb@users.noreply.github.com>
2024-02-14 06:57:53 +0000

    new index added

--- app/backend/app.py
+++ app/backend/app.py
@@ -54,7 +54,7 @@ AZURE_OPENAI_SERVICE_KEY = os.environ.get("AZURE_OPENAI_SERVICE_KEY")
 AZURE_SUBSCRIPTION_ID = os.environ.get("AZURE_SUBSCRIPTION_ID")
 IS_GOV_CLOUD_DEPLOYMENT = str_to_bool.get(os.environ.get("IS_GOV_CLOUD_DEPLOYMENT").lower()) or False
 CHAT_WARNING_BANNER_TEXT = os.environ.get("CHAT_WARNING_BANNER_TEXT") or ""
-APPLICATION_TITLE = os.environ.get("APPLICATION_TITLE") or "Information Assistant"
+APPLICATION_TITLE = os.environ.get("APPLICATION_TITLE") or "Veraqor Information Assistant"
 
 
 KB_FIELDS_CONTENT = os.environ.get("KB_FIELDS_CONTENT") or "content"
@@ -104,7 +104,7 @@ openai.api_key = AZURE_OPENAI_SERVICE_KEY
 # Set up clients for Cognitive Search and Storage
 search_client = SearchClient(
     endpoint=AZURE_SEARCH_SERVICE_ENDPOINT,
-    index_name=AZURE_SEARCH_INDEX,
+    index_name=['AZURE_SEARCH_INDEX','AZURE_SEARCH_INDEX'],
     credential=azure_search_key_credential,
 )
 blob_client = BlobServiceClient(
